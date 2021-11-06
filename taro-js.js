f7316cd768fc4f7060fa3ee440cf95e369eec99f
tonkatutaro <75734754+tonkatutaro@users.noreply.github.com>
2021-11-06 22:05:04 +0900

    Update taro-js.js

--- taro-js.js
+++ taro-js.js
@@ -5,12 +5,37 @@
     const close = document.getElementById('close');
 
     open.addEventListener('click', ()=>{
-        overlay.classList.add('show');
-        open.classList.add('hide');
+        overlay.classList.toggle('show');
+        // open.classList.toggle('hide');
     });
-    close.addEventListener('click', ()=>{
-        overlay.classList.remove('show');
-        open.classList.remove('hide');
+    // close.addEventListener('click', ()=>{
+    //     overlay.classList.remove('show');
+    //     open.classList.remove('hide');
+    // });
+
+    const trigger = document.querySelector('.menu-trigger');
+    const span = document.querySelectorAll('span');
+
+    trigger.addEventListener('click' , ()=>{
+        trigger.classList.toggle('active');
     });
 
+
+
+    function play() {
+        setTimeout(() =>{
+            images[currentIndex].classList.remove("current");
+            currentIndex++;
+            if (currentIndex > images.length - 1) {
+                currentIndex = 0;
+            }
+            images[currentIndex].classList.add("current");
+            play();
+        },3000);
+        
+    } 
+    const images = document.querySelectorAll('.store-picture');
+    let currentIndex = 0;
+    
+    play();
 }
